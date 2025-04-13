925bc3cf3fc51d88862eddb34f5d255747e097de
DAdam03 <domokosadam03@gmail.com>
2025-04-13 15:28:31 +0200

    valtoztatasok

--- src/app/pages/browse-articles/browse-articles.component.ts
+++ src/app/pages/browse-articles/browse-articles.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ArticleCardComponent } from '../../shared/article-card/article-card.component';
 
 @Component({
   selector: 'app-browse-articles',
-  imports: [],
+  imports: [ArticleCardComponent],
   templateUrl: './browse-articles.component.html',
   styleUrl: './browse-articles.component.css'
 })
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, Input} from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatListModule } from '@angular/material/list'
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenav } from '@angular/material/sidenav';
@@ -9,7 +9,6 @@ import { MatSidenav } from '@angular/material/sidenav';
   selector: 'app-menu',
   imports: [
     RouterLink,
-    RouterLinkActive,
     MatListModule,
     MatIconModule
   ],
